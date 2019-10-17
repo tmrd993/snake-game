e9cbf30fc37628acf263008eef461c93c7edc3f0
tmrd993 <47194464+tmrd993@users.noreply.github.com>
2019-10-17 13:41:00 +0200

    Update SnakeController.java

--- SnakeController.java
+++ SnakeController.java
@@ -16,26 +16,26 @@ public class SnakeController implements KeyListener {
 	public void keyReleased(KeyEvent e) 
 	{
 
-	int key = e.getKeyCode();
+		int key = e.getKeyCode();
 
-	//initial position of the head is on the right side of the body,
-        //moving left from starting position would reverse the direction which is not allowed
-        if(key == KeyEvent.VK_LEFT && !(snakeModel.getDirection() == SnakeGameModel.Direction.Right) && !(snakeModel.getDirection() == SnakeGameModel.Direction.None))
-        {
-        	snakeModel.setDirection(SnakeGameModel.Direction.Left);
-        }
-        else if(key == KeyEvent.VK_RIGHT && !(snakeModel.getDirection() == SnakeGameModel.Direction.Left))
-        {
-        	snakeModel.setDirection(SnakeGameModel.Direction.Right);
-        }
+		//initial position of the head is on the right side of the body,
+        	//moving left from starting position would reverse the direction which is not allowed
+        	if(key == KeyEvent.VK_LEFT && !(snakeModel.getDirection() == SnakeGameModel.Direction.Right) && !(snakeModel.getDirection() == SnakeGameModel.Direction.None))
+        	{
+        		snakeModel.setDirection(SnakeGameModel.Direction.Left);
+        	}
+        	else if(key == KeyEvent.VK_RIGHT && !(snakeModel.getDirection() == SnakeGameModel.Direction.Left))
+        	{
+        		snakeModel.setDirection(SnakeGameModel.Direction.Right);
+        	}
         else if(key == KeyEvent.VK_UP && !(snakeModel.getDirection() == SnakeGameModel.Direction.Down))
-        {
-        	snakeModel.setDirection(SnakeGameModel.Direction.Up);
-        }
-        else if(key == KeyEvent.VK_DOWN && !(snakeModel.getDirection() == SnakeGameModel.Direction.Up))
-        {
-        	snakeModel.setDirection(SnakeGameModel.Direction.Down);
-        }
+        	{
+        		snakeModel.setDirection(SnakeGameModel.Direction.Up);
+        	}
+        	else if(key == KeyEvent.VK_DOWN && !(snakeModel.getDirection() == SnakeGameModel.Direction.Up))
+        	{
+        		snakeModel.setDirection(SnakeGameModel.Direction.Down);
+        	}
 	}
 
 	@Override
